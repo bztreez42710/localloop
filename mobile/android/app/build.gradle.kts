82571463d796f3377d7caa4264bcfc13a21eb622
bztreez42710 <htheaqua@gmail.com>
2026-09-16 17:29:46 -0700

    Add Android app module

---
+++ mobile/android/app/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.localloop.driver"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.localloop.driver"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+}
+
+dependencies {
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+}
