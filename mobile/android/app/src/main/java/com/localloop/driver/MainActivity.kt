c0750a2c983258b6d29bc16c81094f9ea8d1e82a
bztreez42710 <htheaqua@gmail.com>
2026-09-19 12:52:02 -0700

    Fix Android visual banner view import

--- mobile/android/app/src/main/java/com/localloop/driver/MainActivity.kt
+++ mobile/android/app/src/main/java/com/localloop/driver/MainActivity.kt
@@ -14,6 +14,7 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Base64
 import android.view.Gravity
+import android.view.View
 import android.widget.*
 import org.json.JSONArray
 import org.json.JSONObject
